5cd58eb56a0cd803bd557f9e87c3379372f9f7a5
Maxim Novak <maximn@wix.com>
2017-06-22 17:33:44 +0100

    remove type annotation from public val in SmartCompletion example I'm making it private in the live coding session.

--- src/main/scala/com/tutorial/idea/SmartCompletion.scala
+++ src/main/scala/com/tutorial/idea/SmartCompletion.scala
@@ -6,7 +6,7 @@ class SmartCompletion {
   // multiline selection
   val i = 1
   val s = "1"
-  val c: Charset = Charset.defaultCharset()
+  val c = Charset.defaultCharset()
   val d = 1.1
   //println using `priv`
 
