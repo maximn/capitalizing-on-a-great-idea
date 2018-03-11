a5aec7e89e274e2ab9b83ba4e53bb9ff1fbb9bbc
Maxim Novak <maximn@wix.com>
2018-03-11 09:27:07 +0200

    Ident bookmarks comment

--- scala/src/main/scala/com/tutorial/idea/Navigation.scala
+++ scala/src/main/scala/com/tutorial/idea/Navigation.scala
@@ -4,7 +4,10 @@ class Navigation {
   // Move between tabs in open order (ALT + CMD + LEFT/RIGHT)
   //                      left/right (CMD + SHIFT + [/])
   // Recently viewed files (CMD + E), recently edited (CMD+SHIFT+E)
-  // Bookmarks (Set - CTRL + SHIFT + NUMBER, Goto - CTRL + NUMBER, List - CMD + F3, Toggle - F3)
+  // Bookmarks (Set - CTRL + SHIFT + NUMBER,
+  //            Goto - CTRL + NUMBER,
+  //            Toggle - F3,
+  //            List - CMD + F3, Toggle - F3)
 
   // Switch between Test/Production code (CMD + SHIFT + T)
 
