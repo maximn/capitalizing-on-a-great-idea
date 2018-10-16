e3fc9ff36172901f296a31bca8c41fdde7b01c0a
Maxim Novak <maximn@wix.com>
2018-10-16 21:12:18 +0300

    Analyze Stacktrace

--- java/src/main/java/App.java
+++ java/src/main/java/App.java
@@ -1,4 +1,5 @@
 import com.tutorial.idea.Lookups;
+import com.tutorial.idea.StackTrace;
 
 import java.util.logging.Logger;
 
@@ -6,6 +7,18 @@ public class App {
     private final static Logger LOGGER = Logger.getLogger(Lookups.class.getName());
 
     public static void main(String[] args) {
-        LOGGER.info(LOGGER.getName() + " some log message");
+        int selection = 0;
+        if (args.length > 0) {
+            selection = Integer.parseInt(args[0]);
+        }
+
+        switch (selection) {
+            case 0:
+                LOGGER.info(LOGGER.getName() + " some log message");
+                break;
+            case 1:
+                new StackTrace().firstMethod();
+                break;
+        }
     }
 }
---
+++ java/src/main/java/com/tutorial/idea/StackTrace.java
@@ -0,0 +1,28 @@
+package com.tutorial.idea;
+
+import java.util.Random;
+
+public class StackTrace {
+    private Random random = new Random();
+
+    public double firstMethod() {
+        return secondMethod();
+    }
+
+    public double secondMethod() {
+        return thirdMethod();
+    }
+
+    public double thirdMethod() {
+        return forthMethod();
+    }
+
+    public double forthMethod() {
+        double number = random.nextInt(10) / 10d;
+        String numericString = String.valueOf(Math.rint(number)); // Expression type (CTRL + SHIFT + P)
+
+        return Integer.parseInt(numericString);
+    }
+}
+
+// How can we change all methods to return int instead? Remove the `Method` suffix? - Add Selection for next Occurrence (CTRL + G)
